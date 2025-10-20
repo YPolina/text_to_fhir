6bd5e07cb920a54369c77b6cf72761fd2a77664a
Palina <palina.yatsko@innowise.com>
2025-10-20 11:29:46 +0200

    Save generated by LLM cases

--- src/utils/llm_utils.py
+++ src/utils/llm_utils.py
@@ -1,5 +1,6 @@
 import re
-
+import yaml
+from pathlib import Path
 
 def extract_json_block(raw_text: str) -> str:
     """
@@ -13,3 +14,32 @@ def extract_json_block(raw_text: str) -> str:
     if match:
         return match.group(0)
     return raw_text
+
+def save_generated_case(disease: str, case_text:str, yaml_path: str = "src/config/generated_cases.yaml"):
+
+    yaml_file = Path(yaml_path)
+    yaml_file.parent.mkdir(parents=True, exist_ok=True)
+
+    if yaml_file.exists():
+        with open(yaml_file, "r") as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+    else:
+        data = {}
+
+    disease_key = disease.lower().replace(" ", "_")
+
+    if disease_key not in data:
+        data[disease_key] = []
+
+    case_id = f"case_{len(data[disease_key]) + 1}"
+    data[disease_key].append({
+        "id": case_id,
+        "text": case_text
+    })
+
+    with open(yaml_file, "w", encoding="utf-8") as f:
+        yaml.dump(data, f, sort_keys=False, allow_unicode=True)
+
+
+
+
