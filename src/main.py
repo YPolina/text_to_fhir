bf168115eaf5b1121a466ee29fca6c998c63c1f4
Palina <palina.yatsko@innowise.com>
2025-10-20 11:30:45 +0200

    Add additional mode for case generation

--- src/main.py
+++ src/main.py
@@ -7,7 +7,9 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import boto3
 import argparse
 from src.core import settings
+from src.services.generation import generate_case
 from src.services.text_to_json import process_patient_records
+from src.utils.llm_utils import save_generated_case
 from src.services.json_to_fhir import to_fhir_bundle
 from src.utils.load import load_config
 import logging
@@ -23,12 +25,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Converter Patient data to FHIR")
     parser.add_argument("--config", type=Path, default=Path("config.yaml"),
                         help="Path to YAML config file")
-    args = parser.parse_args()
-
-    config = load_config(args.config)
-
-    cases = load_config(Path(config["cases_file"]))
-    output_dir = Path(config["output_dir"])
 
     session = boto3.Session(
         aws_access_key_id=settings.AWS_KEY,
@@ -39,6 +35,24 @@ if __name__ == "__main__":
 
     client = session.client("bedrock-runtime")
     logger.info("Bedrock client created")
+
+    args = parser.parse_args()
+
+    config = load_config(args.config)
+
+    mode = config["mode"]
+    if mode == "generate":
+        for disease_entry in config["diseases"]:
+            disease = disease_entry["name"]
+            num_gen = disease_entry.get("num_generation", 1)
+
+            for i in range(num_gen):
+                logger.info(f"Generating case {i + 1} for {disease}...")
+                case_text = generate_case(disease, client, settings.MODEL_ID, logger)
+                if case_text:
+                    save_generated_case(disease, case_text, config["cases_file"])
+    cases = load_config(Path(config["cases_file"]))
+    output_dir = Path(config["output_dir"])
 
     # Process all diseases and cases
     for disease, case_list in cases.items():
@@ -53,7 +67,7 @@ if __name__ == "__main__":
 
             # Run your LLM pipeline on the case text
             llm_output = process_patient_records(case_text, client, settings.MODEL_ID, logger=logger)
-            logger.info(f"LLM output for {case_id}: {llm_output}")
 
             filename = to_fhir_bundle(llm_output, case_id, disease_dir)
             logger.info(f"FHIR bundle saved to {filename}")
+
